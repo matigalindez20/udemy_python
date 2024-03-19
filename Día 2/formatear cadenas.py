6c63cdd372dc2f6db2bd4336bacf9189207d3814
matigalindez20 <matiasgalindez20@gmail.com>
2024-03-19 16:46:00 -0300

    prbadno git

--- Día 2/formatear cadenas.py
+++ Día 2/formatear cadenas.py
@@ -9,3 +9,4 @@ puntos_nuevos = 350
 puntos_totales = puntos_anteriores + puntos_nuevos
 print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores + puntos_nuevos} puntos")
 print("Hola mundo")
+print("probando git")
