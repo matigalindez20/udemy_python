92347c441384d95596c32d69ad3d93721b33836e
matigalindez20 <matiasgalindez20@gmail.com>
2024-03-19 16:28:32 -0300

    cambios en formatear cadenas

--- Día 2/formatear cadenas.py
+++ Día 2/formatear cadenas.py
@@ -8,3 +8,4 @@ puntos_anteriores = 875
 puntos_nuevos = 350
 puntos_totales = puntos_anteriores + puntos_nuevos
 print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores + puntos_nuevos} puntos")
+print("Hola mundo")
