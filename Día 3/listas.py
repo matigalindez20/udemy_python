f28129c838e912d52c76c9bb90ae6a194b349ca1
matigalindez20 <matiasgalindez20@gmail.com>
2024-03-21 13:44:40 -0300

    día 3 -> diccionarios

---
+++ Día 3/listas.py
@@ -0,0 +1,24 @@
+my_list = ["a", "b", "c"]
+my_list2 = ["d", "f", "g"]
+#print(type(my_list))
+#print(len(my_list))
+resultado = my_list[0:2]
+#print(resultado)
+
+my_list3 = my_list + my_list2
+
+my_list3[0] = "z" #cambiar el contendio del indice 0 y remplaza por el de comillas
+
+my_list3.append("agregando") #agregar elemento al último
+my_list3.pop(0) # cuando esta vacío elimina el último elemento
+print(my_list3)
+
+
+lista = ["f", "m", "v", "a"]
+lista.sort() #no se puede asignar a una variable el la lista ordenada -> si lo haces devuelte un nonetype
+# Se puede hacer lo siguiente ->
+lista2 = lista
+print(lista)
+print(lista2)
+lista2.reverse()
+print(lista2)
---
+++ Día 3/propiedades de strings.py
@@ -0,0 +1,12 @@
+name = "matias"
+name = "M"
+print(name * 10) # nos multiplica 10 veces el string name
+
+poema = """Esta propiedad
+sirve para hacer
+saltos de líneas
+sin usar la n"""
+print("líneass" in poema) # si la palabra se encuentra dentro de poeama
+print("porque" not in poema) # si la palabra NO se encuentra
+print(len(poema)) # nos dice el largo que tiene poema
+
